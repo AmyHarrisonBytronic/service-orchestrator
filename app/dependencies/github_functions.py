68ee1855d5ebd6ed2d9c05c166446175e25305a1
Amy <you@example.com>
2026-09-03 16:55:49 +0100

    added a directory management class

---
+++ app/dependencies/github_functions.py
@@ -0,0 +1,6 @@
+class GithubActions():
+    '''a class to manage github actions like downloading files 
+    from release pages and other associated internet actions'''
+
+    def __init__(self):
+        pass
